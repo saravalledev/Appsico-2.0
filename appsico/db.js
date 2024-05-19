484315d5d0003846df5b8ffc9ae0e005af276e3b
HebertVS <kira01010110@gmail.com>
2024-05-19 03:38:24 -0300

    Refatorando código guiado pelo sênior para mudança de tecnologias candidatas a solução do desenvolvimente em menor prazo e maior eficiencia. Em caso de dúvidas pela equipe, por favor entrar em contato com a lider do projeto: Karine Sousa.

--- appsico/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('dbappsico', 'root', 'appsico', {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306
-})
-
-module.exports = sequelize;
--- appsico/public/javascripts/helpers.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  renderMenuExterno: (caminhoMenu) => {
-    return res.render(path.join(__dirname, '../', caminhoMenu));
-  },
-};
